baa41cdafc8419b6a99f0b3298fdee69c2904751
WeiLocus <marsuo52@gmail.com>
2023-03-07 21:41:18 +0800

    add useEffect to get todos from api

--- src/pages/TodoPage.jsx
+++ src/pages/TodoPage.jsx
@@ -1,53 +1,34 @@
 import { Footer, Header, TodoCollection, TodoInput } from 'components';
 import { useState } from 'react'
-
-const dummyTodos = [
-  {
-    title: 'Learn react-router',
-    isDone: true,
-    id: 1,
-  },
-  {
-    title: 'Learn to create custom hooks',
-    isDone: false,
-    id: 2,
-  },
-  {
-    title: 'Learn to use context',
-    isDone: true,
-    id: 3,
-  },
-  {
-    title: 'Learn to implement auth',
-    isDone: false,
-    id: 4,
-  },
-];
+import { useEffect } from 'react'
+import { getTodos } from '../api/todos'
 
 const TodoPage = () => {
-  const [inputValue,setInputValue] = useState('')
-  const [todos,setTodos] = useState(dummyTodos)
+  const [inputValue, setInputValue] = useState('');
+  //刪除dummyTodos改成axios取得資料
+  const [todos, setTodos] = useState([]);
 
   //監聽InputValue事件
   const handleChange = (value) => {
-    setInputValue(value)
-  }
+    setInputValue(value);
+  };
 
   //監聽按下新增看增加todo -> onAddTodo
   const handleAddTodo = () => {
-    if (inputValue.length === 0) return
+    if (inputValue.length === 0) return;
     setTodos((prevTodos) => {
       return [
         ...prevTodos,
-      {
-        id: Math.random()*100,
-        title: inputValue,
-        isDone: false
-      }]
-    })
+        {
+          id: Math.random() * 100,
+          title: inputValue,
+          isDone: false,
+        },
+      ];
+    });
     //新增資料後清空輸入框
-    setInputValue('')
-  }
+    setInputValue('');
+  };
 
   //監聽todoInput的onkeyDown事件
   const handleKeyDown = (event) => {
@@ -64,74 +45,91 @@ const TodoPage = () => {
     });
     //新增資料後清空輸入框
     setInputValue('');
-  }
+  };
 
   //監聽todoItem的icon，切換完成狀態
   const handleToggleDone = (id) => {
     setTodos((prevTodos) => {
-      return prevTodos.map(todo => {
+      return prevTodos.map((todo) => {
         if (todo.id === id) {
           return {
             ...todo,
-            isDone: !todo.isDone
-          }
+            isDone: !todo.isDone,
+          };
         }
-        return todo
-      })
-    })
-  }
+        return todo;
+      });
+    });
+  };
 
   //監聽todoItem的onDoubleClick修改內容事件
-  const handleChangeMode = ({id, isEdit}) => {
+  const handleChangeMode = ({ id, isEdit }) => {
     setTodos((prevTodos) => {
-      return prevTodos.map(todo => {
-        if ( todo.id === id)  {
+      return prevTodos.map((todo) => {
+        if (todo.id === id) {
           return {
-            ...todo, isEdit
-          }
+            ...todo,
+            isEdit,
+          };
         }
         // 其他沒有被點擊的isEdit是false
-        return {...todo,isEdit:false}
-      })
-    })
-  }
-  
+        return { ...todo, isEdit: false };
+      });
+    });
+  };
+
   //監聽onSave事件，拿到id和修改的title
-  const handleSave = ({id, title}) => {
+  const handleSave = ({ id, title }) => {
     setTodos((prevTodos) => {
-      return prevTodos.map(todo => {
-        if( todo.id === id ) {
+      return prevTodos.map((todo) => {
+        if (todo.id === id) {
           return {
             ...todo,
             title,
-            isEdit: false
+            isEdit: false,
           };
         }
-        return todo
-      })
-    })
-  }
+        return todo;
+      });
+    });
+  };
 
   //監聽onDelete事件，刪除todo
   const handleDelete = (id) => {
-    setTodos((prevTodos) => prevTodos.filter(todo => todo.id !== id)
-    )
-  }
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+  };
+
+  //useEffect搭配getTodos，在畫面渲染後取得後端資料，拿到所有todos項目，更新todos的state
+  useEffect(() => {
+    const getTodosAsync = async () => {
+      try {
+        const todos = await getTodos();
+        setTodos(todos.map((todo) => ({ ...todo, isEdit: false })));
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    //執行
+    getTodosAsync();
+  }, []);
   return (
     <div>
       TodoPage
       <Header />
-      <TodoInput 
+      <TodoInput
         inputValue={inputValue}
         onChange={handleChange}
         onAddTodo={handleAddTodo}
-        onKeyDown={handleKeyDown}/>
-      <TodoCollection todos={todos}
+        onKeyDown={handleKeyDown}
+      />
+      <TodoCollection
+        todos={todos}
         onToggleDone={handleToggleDone}
         onChangeMode={handleChangeMode}
         onSave={handleSave}
-        onDelete={handleDelete}/>
-      <Footer todos={todos}/>
+        onDelete={handleDelete}
+      />
+      <Footer todos={todos} />
     </div>
   );
 };
