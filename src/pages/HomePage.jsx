2dd251bf16777a92e6e3ad41d028d049dedff8e5
WeiLocus <marsuo52@gmail.com>
2023-03-10 13:27:19 +0800

    feat: add home page navigation

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,18 @@
+import { useAuth } from '../contexts/AuthContext'
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+
 const HomePage = () => {
+  const navigate = useNavigate()
+  const { isAuthenticated } = useAuth()
+
+  useEffect (() => {
+    if (isAuthenticated) {
+      navigate('.todo')
+    } else {
+      navigate('/login')
+    }
+  },[navigate, isAuthenticated])
   return <div>HomePage</div>;
 };
 
